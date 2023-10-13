d1d42f05e6d6c634a36741786027a57d15719cbb
aksel_rag <aksellmao@protonmail.com>
2023-10-13 23:31:45 +0100

    Responsive issues

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ toggleButton.addEventListener('click', function() {
   menu.innerHTML = `
     <div class="sidebar-content">
       <a hreg="" id="rules">RULES</a><br>
-      <a id="keys" >KEY BINDS</a><br>
-      <a id="staff" >STAFF</a><br>
-      <a id="socials" >SOCIALS</a>
+      <a id="keys">KEY BINDS</a><br>
+      <a id="staff">STAFF</a><br>
+      <a id="socials">SOCIALS</a>
     </div>
-    <svg id="shut" xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' style="position: absolute;"><line x1='0' y1='0' x2='100%' y2='350%' stroke='#fff' stroke-width='20%'/></svg>
+    <svg id="shut" xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' style="position: absolute;"><line x1='0' y1='0' x2='100%' y2='350%' stroke='#121212' stroke-width='20%'/></svg>
   `;
 
   let rules = menu.querySelector("#rules");
@@ -46,8 +46,8 @@ toggleButton.addEventListener('click', function() {
   
   toggleButton.addEventListener("click", () =>  {
     main.innerHTML = `
-    <p class="fade-in" style="font-size: 14px;margin-bottom:-50px;">Drifting Server</p>
-    <h1 class="fade-in" style="letter-spacing: 3px;font-family: 'Roboto'">UNIDRIFT</h1>
+    <p class="fade-in" style="font-size: 14px;margin-bottom:-50px;">Fivem Server</p>
+    <h1 class="fade-in" style="letter-spacing: 3px;font-family: 'Roboto'">SERVER NAME</h1>
     <p class="fade-in" style="font-size: 14px;margin-top:-50px;">Loading...</p>
     `;
   });
